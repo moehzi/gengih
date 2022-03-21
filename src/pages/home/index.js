04f8f0d326d4d62b8cfdc56ea7a6aa547732e6fd
moehzi <faizi1234098@gmail.com>
2022-03-21 20:47:35 +0800

    hands-on: module-2-session-1

---
+++ src/pages/home/index.js
@@ -0,0 +1,14 @@
+export const Home = () => {
+  return (
+    <div>
+      <input /> <button>Search</button>
+      <div>
+        <img
+          src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif
+"
+          alt="foto-aja"
+        />
+      </div>
+    </div>
+  );
+};
