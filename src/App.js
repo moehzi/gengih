ecc7f4e069d1d373d0d0eb616ce5f7edce2ef41f
moehzi <faizi1234098@gmail.com>
2022-04-02 21:53:22 +0800

    chore: init tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  const giphy_secret_key = process.env.REACT_APP_GIPHY_KEY;
-  console.log(giphy_secret_key);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {giphy_secret_key}
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
